774ba792b936f09dbaa822eacdff8c24014a67a6
01ebba <01ebbaandersson@gmail.com>
2026-09-02 18:59:45 +0200

    create and list instruments + starting on a menu in main

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ from entities import Instrument
 from entities import Trade
 
 portfolios = []
+instruments = []
 
 
 # Create portfolios
@@ -21,8 +22,22 @@ def list_portfolios():
     for portfolio in portfolios:
         print(f"{portfolio.name} - {portfolio.currency}")
 
-# Create and list instruments
 
+# Create instrument
+def create_instrument():
+    name = input("Name: ")
+    instrument_type = input("Instrument type: ")
+
+    instrument = Instrument(name, instrument_type)
+    instruments.append(instrument)
+
+    print("Instrument created")
+
+
+# List instruments
+def list_instruments():
+    for instrument in instruments:
+        print(f"{instrument.name} - {instrument.instrument_type}")
 
 # Record a trade (buy or sell) for a given instrument and portfolio
 # List all trades, with optional filtering by portfolio or instrument
--- main.py
+++ main.py
@@ -1,2 +1,26 @@
+from functions import create_portfolio, list_portfolios, create_instrument, list_instruments
+
+running = True
+
+while running:
+    print("""
+        1. Create portfolio
+        2. List portfolios
+        3. Create instrument
+        4. List instruments
+        x. Quit
+    """)
 
 
+    choice = input("Choose: ")
+
+    if choice == "1":
+        create_portfolio()
+    elif choice == "2":
+        list_portfolios()
+    elif choice == "3":
+        create_instrument()
+    elif choice == "4":
+        list_instruments()
+    elif choice == "x":
+        running = False
